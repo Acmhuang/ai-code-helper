c3ebddb74522dd52a0116c84b5a770ca5634953b
chengming <118154550+Acmhuang@users.noreply.github.com>
2025-09-14 14:52:20 +0800

    多模态与系统提示词

--- src/main/java/com/acmhuang/ai/aicodehlper/ai/AiCodeHelper.java
+++ src/main/java/com/acmhuang/ai/aicodehlper/ai/AiCodeHelper.java
@@ -1,6 +1,7 @@
 package com.acmhuang.ai.aicodehlper.ai;
 
 import dev.langchain4j.data.message.AiMessage;
+import dev.langchain4j.data.message.SystemMessage;
 import dev.langchain4j.data.message.UserMessage;
 import dev.langchain4j.model.chat.ChatModel;
 import dev.langchain4j.model.chat.response.ChatResponse;
@@ -20,8 +21,26 @@ public class AiCodeHelper {
     @Resource
     private ChatModel qwenChatModel;
 
+    private static final String SYSTEM_MESSAGE = """
+                    你是编程领域的小助手，帮助用户解答编程学习和求职面试相关的问题，并给出建议，重点关注4个方向
+                    1. 规划清晰的编程学习路线
+                    2. 提供项目学习建议
+                    3. 给出程序员求职全流程指南（比如简历优化、投递技巧）
+                    4. 分享高频面试题和面试技巧
+                    请用简洁易懂的语言问答，助力用户高效学习和求职
+            """;
+
+    //简单对话
     public String chat(String message){
+        SystemMessage systemMessage = SystemMessage.from(SYSTEM_MESSAGE);
         UserMessage userMessage = UserMessage.from(message);
+        ChatResponse chatResponse = qwenChatModel.chat(systemMessage,userMessage);
+        AiMessage aiMessage = chatResponse.aiMessage();
+        log.info("aiMessage: {}", aiMessage);
+        return aiMessage.text();
+    }
+    //简单对话 - 自定义用户消息
+    public String chat(UserMessage userMessage){
         ChatResponse chatResponse = qwenChatModel.chat(userMessage);
         AiMessage aiMessage = chatResponse.aiMessage();
         log.info("aiMessage: {}", aiMessage);
--- src/test/java/com/acmhuang/ai/aicodehlper/AiCodeHlperApplicationTests.java
+++ src/test/java/com/acmhuang/ai/aicodehlper/AiCodeHlperApplicationTests.java
@@ -1,9 +1,14 @@
 package com.acmhuang.ai.aicodehlper;
 
 import com.acmhuang.ai.aicodehlper.ai.AiCodeHelper;
+import dev.langchain4j.data.message.ImageContent;
+import dev.langchain4j.data.message.TextContent;
+import dev.langchain4j.data.message.UserMessage;
 import jakarta.annotation.Resource;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+
+import java.awt.*;
 
 @SpringBootTest
 class AiCodeHlperApplicationTests {
@@ -14,6 +19,15 @@ class AiCodeHlperApplicationTests {
     @Test
     void chat() {
         aiCodeHelper.chat("你好我是一个正在学习langchain4j的Java程序员");
+    }
+
+    @Test
+    void testChat(){
+        UserMessage userMessage = UserMessage.from(
+                TextContent.from("描述图片"),
+                ImageContent.from("https://i0.hdslb.com/bfs/new_dyn/edc297f0e77c71348bf24cff4c2174fb253428607.gif@480w_480h.avif")
+        );
+        aiCodeHelper.chat(userMessage);
     }
 
 }
